ec5fbab0ff8818880b98a9dde19c7bd9b5ba8463
ismail <wibidjokam@gmail.com>
2022-07-24 14:24:07 +0700

    Setup Frontend

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,39 @@ import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
+import CssBaseLine from '@mui/material/CssBaseline'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import AppLayout from './components/layout/AppLayout'
+import AuthLayout from './components/layout/AuthLayout'
+import Home from './pages/Home'
+import Board from './pages/Board'
+import Signup from './pages/Signup'
+import Login from './pages/Login'
 
 function App() {
+  const theme = createTheme({
+    palette: { mode: 'dark' }
+  })
+
   return (
-    <div>
-      Kanban app
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseLine>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<AuthLayout/>}>
+              <Route path='login' element={<Login/>}></Route>
+              <Route path='signup' element={<Signup/>}></Route>
+            </Route>
+            <Route path='/' element={<AppLayout/>}>
+              <Route index element={<Home/>}></Route>
+              <Route path='boards' element={<Home/>}></Route>
+              <Route path='boards/:boardId' element={<Board/>}></Route>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </CssBaseLine>
+    </ThemeProvider>
   );
 }
 
