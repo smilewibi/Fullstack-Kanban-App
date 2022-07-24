b915f642a0d0361046ddfeebd7f374003e2e12aa
ismail <wibidjokam@gmail.com>
2022-07-24 20:58:31 +0700

    Config Axios

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,7 @@
 const assets = {
     images: {
         logoDark: require('./images/logo-dark.png'),
-        appIcon: require('.images/favicon.png')
+        appIcon: require('./images/favicon.png')
     },
     colors: {
         secondary: '#292929'
