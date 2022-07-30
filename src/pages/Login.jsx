0ee14aee4844b1c60abb0af0e77bf8bc93ec22aa
ismail <wibidjokam@gmail.com>
2022-07-30 18:59:21 +0700

    Login - Register Page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,57 @@
-import React from 'react'
-
+import { Box, Button, TextField } from '@mui/material'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import LoadingButton from '@mui/lab/LoadingButton'
 const Login = () => {
+    const [loading, setLoading] = useState(false)
+
+    const handleSubmit = () => {
+        //
+    }
     return (
-        <div>Login</div>
+        <>
+        <Box 
+        component='form'
+        sx={{ mt:1 }}
+        onSubmit={handleSubmit}
+        noValidate>
+            <TextField
+                margin='normal'
+                required
+                fullWidth
+                id='username'
+                label='Username'
+                name='username'
+                disabled={loading}
+            />
+            <TextField
+                margin='normal'
+                required
+                fullWidth
+                id='password'
+                label='Password'
+                name='password'
+                type='password'
+                disabled={loading} />
+            <LoadingButton
+                sx={{ mt: 3, mb: 2 }}
+                variant='outlined'
+                fullWidth
+                color='success'
+                type='submit'
+                loading={loading}
+            >
+                Login
+            </LoadingButton>
+            <Button
+                component={Link}
+                to='/signup'
+                sx={{ textTransform: 'none' }}
+            >
+                Don't have an account? Signup
+            </Button>
+        </Box>
+        </>
     )
 }
 
